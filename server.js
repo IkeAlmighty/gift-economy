a406c3741241bd289c818bce5e647351283c8dcf
ikealmighty <isaacyates7@gmail.com>
2025-07-30 17:10:45 -0500

    added server code, removed .env from tracking

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const authRoutes = require("./routes/auth");
+const apiRoutes = require("./routes/api");
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB error", err));
+
+app.use("/auth", authRoutes);
+app.use("/api", apiRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
