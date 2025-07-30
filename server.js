8447b73485349253202d8ef6510c023a32ae8abd
ikealmighty <isaacyates7@gmail.com>
2025-07-30 17:38:52 -0500

    added dependencies, removed old mongoose options

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB error", err));
 
