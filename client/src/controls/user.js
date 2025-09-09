89361a204da79252c7f91a9d2ef2a79e459c10de
ikealmighty <isaacyates7@gmail.com>
2025-09-08 22:16:44 -0500

    refractored API to 'controls', added some user login control interfaces

---
+++ client/src/controls/user.js
@@ -0,0 +1,33 @@
+// NOTE: Use the UserContext for logging in and logging out. This code
+// is called by the context's respective functions.
+
+export async function login({ username, password }) {
+  const response = await fetch("/api/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  const data = await response.json();
+  console.log(data);
+
+  return { data, error: data.error };
+}
+
+export async function logout() {}
+
+export async function signup() {}
+
+export async function me() {
+  try {
+    const response = await fetch("/api/auth/me");
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+  } catch (err) {
+    return null;
+  }
+}
