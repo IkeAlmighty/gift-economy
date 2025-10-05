bb0421a2bc57184dbe132349a595ac3062351aec
ikealmighty <isaacyates7@gmail.com>
2025-10-04 20:07:30 -0500

    added default values

--- client/src/components/modalMenus/CreateRequestMenu.jsx
+++ client/src/components/modalMenus/CreateRequestMenu.jsx
@@ -1,36 +1,42 @@
 import { useState } from "react";
 
-export default function CreateRequestMenu({ onAction, prefill = {} }) {
-  const [description, setDescription] = useState("");
+export default function CreateGiftMenu({ onAction, formData = {} }) {
+  const [description, setDescription] = useState(formData.description || "");
   const MAX_DESCR_CHAR = 200;
 
   function handleFormSubmit(e) {
     e.preventDefault();
 
-    const data = new FormData(e.target);
+    const newFormData = new FormData(e.target);
+    newFormData.append("superType", "Request");
 
-    onAction({ nextMenu: "PreviewListingMenu", data });
+    if (formData.imageUrl && !newFormData.image) newFormData.append("imageUrl", formData.imageUrl);
+
+    onAction({ nextMenu: "PreviewListingMenu", formData: newFormData });
   }
+
   return (
     <div>
       <h2 className="mb-5">Post a Request 🪫</h2>
-
       <form className="flex flex-col space-y-5" onSubmit={handleFormSubmit}>
         <label>
           <div>Title: </div>
-          <input type="text" name="title" />
+          <input type="text" name="title" defaultValue={formData.title} />
         </label>
-
         <label>
           <div>Listing Type(s):</div>
           {["Food", "Shelter", "Labor", "Transportation", "Other"].map((t) => (
-            <label>
-              <input className="mr-1" type="checkbox" name={`Type-${t}`} />
+            <label key={t}>
+              <input
+                className="mr-1"
+                type="checkbox"
+                name={`Type-${t}`}
+                defaultChecked={formData[`Type-${t}`]}
+              />
               <span className="mr-5">{t}</span>
             </label>
           ))}
         </label>
-
         <div>Description:</div>
         <textarea
           className="min-h-[200px] border-2 rounded p-2"
@@ -42,18 +48,13 @@ export default function CreateRequestMenu({ onAction, prefill = {} }) {
         <div>
           {description.length} / {MAX_DESCR_CHAR}
         </div>
-
         <div>
-          <div>Select an image:</div>
-          <input
-            name="image"
-            type="file"
-            accept="image/png, image/jpg, image/jpeg"
-          />
+          <div>{formData.imageUrl ? "Change image:" : "Select an image:"}</div>
+          <input name="image" type="file" accept="image/png, image/jpg, image/jpeg" />
         </div>
 
         <div>
-          <input type="submit" value="Post" />
+          <input type="submit" value="Preview" />
         </div>
       </form>
     </div>
