e86fc8e94744ce20cafb22d513dde3f9b59a93f3
ikealmighty <isaacyates7@gmail.com>
2025-10-24 22:43:48 -0500

    added new userRouter

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
-import authRouter from "./userRouter.js";
+import authRouter from "./authRouter.js";
 import listingsRouter from "./listingsRouter.js";
+import userRouter from "./userRouter.js";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/listings", authMiddleware, listingsRouter);
+router.use("/user", authMiddleware, userRouter);
 
 export default router;
