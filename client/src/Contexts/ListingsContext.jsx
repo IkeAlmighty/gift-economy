864acc07a7d374b01b43bac76ac81cfee38d7580
ikealmighty <isaacyates7@gmail.com>
2025-11-03 20:42:01 -0600

    created an importable 'hydrateListings' method, added a 'myListings' variable

--- client/src/Contexts/ListingsContext.jsx
+++ client/src/Contexts/ListingsContext.jsx
@@ -1,21 +1,28 @@
-import { useState, useEffect, createContext } from "react";
-import { getListingsInNetwork } from "../controls/listings";
+import { useState, useEffect, createContext, useContext } from "react";
+import { getListingsInNetwork, getMyListings } from "../controls/listings";
+
 export const ListingsContext = createContext(null);
 
 export function ListingsProvider({ children }) {
   const [inNetworkListings, setInNetworkListings] = useState([]);
+  const [myListings, setMyListings] = useState([]);
 
   useEffect(() => {
-    // hydrate listings from backend
-    (async () => {
-      const _inNetworkListings = await getListingsInNetwork();
-
-      setInNetworkListings(_inNetworkListings);
-    })();
+    hydrateListings();
   }, []);
 
+  async function hydrateListings() {
+    const _inNetworkListings = await getListingsInNetwork();
+    const _myListings = await getMyListings();
+
+    setInNetworkListings(_inNetworkListings);
+    setMyListings(_myListings);
+  }
+
   return (
-    <ListingsContext.Provider value={{ inNetworkListings }}>{children}</ListingsContext.Provider>
+    <ListingsContext.Provider value={{ inNetworkListings, myListings, hydrateListings }}>
+      {children}
+    </ListingsContext.Provider>
   );
 }
 
