cfce72f3ad13886e3d1a2476a24d4559caa6b8a8
ikealmighty <isaacyates7@gmail.com>
2025-11-03 21:01:44 -0600

    create ListingsPage component

---
+++ client/src/pages/SavedListingsPage.jsx
@@ -0,0 +1,36 @@
+import ToolBar from "../components/ToolBar";
+import LogoutButton from "../components/LogoutButton";
+import { Link } from "react-router";
+import { useListingsData } from "../Contexts/ListingsContext";
+import { useEffect } from "react";
+
+export function SavedListingsPage() {
+  const { myListings } = useListingsData();
+
+  return (
+    <div>
+      <ToolBar>
+        <span />
+        <button>
+          <Link to="/connections">Connections</Link>
+        </button>
+
+        <button>
+          <Link to="/">Back to Feed</Link>
+        </button>
+
+        <LogoutButton />
+      </ToolBar>
+      <div className="px-2">
+        <h1>Your Listings</h1>
+
+        {myListings.map((listing) => (
+          <div className="border-b-2 flex [&>*]:mx-2">
+            <div>{listing.title}</div>
+            <div>{listing.intent}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
