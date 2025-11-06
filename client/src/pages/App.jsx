bcde85b7621deeed2cb68c56eb30b2e44af6faa9
ikealmighty <isaacyates7@gmail.com>
2025-11-05 19:03:35 -0600

    removed unnessecary import

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router";
 
 import ListItem from "../components/ListItem";
 import { getListingsInNetwork } from "../controls/listings";
