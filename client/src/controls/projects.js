7003be9ce23566dabed3fb7f6000611a4494244a
ikealmighty <isaacyates7@gmail.com>
2025-11-21 11:25:42 -0600

    deleted projects controller as projects are controlled like any other listing

--- client/src/controls/projects.js
+++
@@ -1,7 +0,0 @@
-export async function createProject(projectData) {}
-
-export async function getProjectsInNetwork() {}
-
-export async function editProject({ projectId, newData }) {}
-
-export async function deleteProject({ projectId }) {}
