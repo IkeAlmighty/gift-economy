8bd588d624564f6e9ec6703ffd2e1bf4bfe931ef
ikealmighty <isaacyates7@gmail.com>
2025-12-08 13:42:05 -0600

    created a mostly blank chat client page component

---
+++ client/src/components/ChatClient.jsx
@@ -0,0 +1,8 @@
+export default function ChatClient({ listingId }) {
+  return (
+    <>
+      <div className="py-3">Chat Client {listingId}</div>
+      {/* Chat client implementation goes here */}
+    </>
+  );
+}
