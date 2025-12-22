7a4445661c2ea65ee757e631b4e95b1369f74b1d
ikealmighty <isaacyates7@gmail.com>
2025-12-22 12:58:54 -0600

    updated seeds to reflect the new tags and screenNames

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -1,7 +1,10 @@
 import User from "../models/User.js";
 import Notification from "../models/Notification.js";
 import Listing from "../models/Listing.js";
+import Tag from "../models/Tag.js";
 import userData from "./seedUsers.js";
+import { listingTitles } from "./seedListings.js";
+import tagData from "./seedTags.js";
 import db from "../db/connection.js";
 
 db.once("open", seed);
@@ -12,8 +15,14 @@ async function seed() {
     await User.deleteMany({});
     await Notification.deleteMany({});
     await Listing.deleteMany({});
+    await Tag.deleteMany({});
 
     console.log("Existing data cleared.");
+
+    // Create default tags
+    for (const tag of tagData) {
+      await Tag.create({ name: tag.name, emoji: tag.emoji });
+    }
 
     // Create test users
     for (const user of userData) {
@@ -23,6 +32,20 @@ async function seed() {
     // Connect some users to eachother
     for (let i = 0; i < userData.length; i++) {
       const user = await User.findOne({ username: userData[i].username });
+
+      // for each user add a couple listings:
+      for (let k = 0; k < Math.random() * 3; k++) {
+        const listingData = listingTitles[Math.floor(Math.random() * listingTitles.length)];
+        const listing = await Listing.create({
+          title: listingData.title,
+          description: listingData.description,
+          intent: ["GIFT", "REQUEST", "PROJECT"][Math.floor(Math.random() * 3)],
+          tags: listingData.tags,
+          creator: user._id,
+        });
+        user.listings.addToSet(listing);
+      }
+
       for (let j = 0; j < Math.random() * userData.length; j++) {
         const randomIndex = Math.floor(Math.random() * userData.length);
         const connection = await User.findOne({ username: userData[randomIndex].username });
