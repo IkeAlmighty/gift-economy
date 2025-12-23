df14ce16efe06211cb215f1d1b3c12a67bc6f008
ikealmighty <isaacyates7@gmail.com>
2025-12-23 13:26:44 -0600

    added handling suggestion actions to router

--- client/src/pages/SingleListingPage.jsx
+++ client/src/pages/SingleListingPage.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router";
-import { getListingById } from "../endpoints/listings";
+import { getListingById, handleSuggestionAction } from "../endpoints/listings";
 import ToolBar from "../components/ToolBar";
 import ChatClient from "../components/ChatClient";
-import ListItem from "../components/ListItem";
 import { getSafeConnectionDataById } from "../endpoints/user";
+import ListingsList from "../components/ListingsList";
 
 export function SingleListingPage() {
   const { _id } = useParams();
@@ -30,85 +30,93 @@ export function SingleListingPage() {
     if (_id) fetchAndSetListingData();
   }, [_id]);
 
-  async function fetchAndSetCreatorData() {
-    const res = await getSafeConnectionDataById(listingData.creator);
-    if (res.ok) {
-      const data = await res.json();
-      setCreator(data);
-    } else {
-      const { error } = await res.json();
-      setErrorMessage(error);
-    }
-  }
-
   useEffect(() => {
     if (!listingData) return;
+    async function fetchAndSetCreatorData() {
+      const res = await getSafeConnectionDataById(listingData.creator);
+      if (res.ok) {
+        const data = await res.json();
+        setCreator(data);
+      } else {
+        const { error } = await res.json();
+        setErrorMessage(error);
+      }
+    }
     fetchAndSetCreatorData();
   }, [listingData]);
+
+  async function _handleSuggestionAction(suggestionId, action) {
+    const res = await handleSuggestionAction(listingData._id, suggestionId, action);
+
+    if (res.ok) {
+      const updatedListing = await getListingById({ _id: listingData._id });
+      setListingData(await updatedListing.json());
+    }
+  }
 
   if (errorMessage) return <>{errorMessage}</>;
   return (
     <div>
       <ToolBar />
 
-      <div className="p-1">
+      <div className="px-3 max-w-[900px] mx-auto">
         <div className="mt-5">
           <span className="text-4xl">{listingData.title}</span> |{" "}
           <span className="text-sm">{listingData.intent}</span>
         </div>
         <span>created by {creator?.username}</span>
 
-        <div className="flex flex-wrap flex-row max-auto justify-between [&>*]:min-w-1/2 my-5 gap-y-10 border-t-2 pt-5 border-gray-200">
-          <div className="flex-1 flex-grow">
+        <div className="flex flex-wrap flex-row mx-auto justify-between my-5 gap-y-10 border-t-2 pt-5 border-gray-200">
+          <div className="flex-grow">
             <h3>Description</h3>
-            {listingData.description}
+            <div className="w-[300px]">{listingData.description}</div>
           </div>
-          <div className="relative text-center border-2 rounded min-h-[400px] flex-grow">
+          <div className="relative text-center border-2 rounded min-h-[400px] min-w-[300px] flex-grow">
             <div className="absolute -top-5 -right-1 bg-secondary p-1 rounded opacity-90 hover:opacity-100">
               <Link to={`/chat?listing=${listingData._id}`} target="_blank">
                 Open in new Tab ↗
               </Link>
             </div>
+
             <ChatClient listingId={listingData._id} />
           </div>
         </div>
 
-        {listingData?.listingsSuggestions && (
-          <h2 className="mt-10 text-center underline">Suggested to this Project</h2>
+        {listingData?.listingsSuggestions?.length > 0 && (
+          <>
+            <h2 className="mt-10 text-center underline">Suggested to this Project</h2>
+            <div className="flex flex-row flex-wrap gap-x-2 gap-y-5 pt-5 justify-center text-center">
+              <ListingsList
+                listings={listingData?.listingsSuggestions}
+                onActionSet={[
+                  {
+                    actionText: "Accept",
+                    onAction: (listing) => _handleSuggestionAction(listing._id, "accept"),
+                  },
+                  {
+                    actionText: "Deny",
+                    onAction: (listing) => _handleSuggestionAction(listing._id, "deny"),
+                  },
+                ]}
+              />
+            </div>
+          </>
         )}
-
-        <div className="flex flex-row flex-wrap gap-x-2 gap-y-5 pt-5">
-          {listingData?.listingsSuggestions?.map((suggestion) => {
-            return (
-              <div key={suggestion._id || suggestion.id} className="mx-auto">
-                <ListItem data={suggestion} />
-                <div className="[&>button]:p-1 [&>button]:rounded flex flex-row justify-around">
-                  {/* TODO: create handlers for buttons */}
-                  <button className="bg-green-300 w-[100px]">Accept</button>
-                  <button className="bg-red-300 w-[100px]">Deny</button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
 
         {listingData?.listings.length > 0 && (
-          <h2 className="mt-10 text-center underline">Confirmed Project Components</h2>
-        )}
+          <>
+            <h2 className="mt-10 text-center underline">Confirmed Project Components</h2>
 
-        <div className="flex flex-row gap-x-1">
-          {listingData?.listings?.map((suggestion) => {
-            return (
-              <div key={suggestion._id || suggestion.id} className="mx-auto">
-                <ListItem data={suggestion} />
-                <div className="text-center">
-                  {/* TODO: create handler for button */}
-                  <button className="p-1 rounded border-2">Remove from Project</button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+            <div className="flex flex-row gap-x-1 justify-center text-center">
+              <ListingsList
+                listings={listingData?.listings}
+                onActionSet={[
+                  { actionText: "Remove", onAction: (listing) => console.log("Remove", listing) },
+                ]}
+              />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
