b888c8831a7c2b121dfe73e540f1e87431f35111
ikealmighty <isaacyates7@gmail.com>
2026-01-31 21:53:20 -0600

    removed endings on files to make electron forge happy

--- electron/src/main.js
+++ electron/src/main.js
@@ -1,8 +1,8 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 import path from "node:path";
 import started from "electron-squirrel-startup";
-import { initBaseController } from "./controller/index.js";
-import RelayService from "./application/services/fastsync-service.js";
+import { initBaseController } from "./controller";
+import RelayService from "./application/services/fastsync-service";
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (started) {
