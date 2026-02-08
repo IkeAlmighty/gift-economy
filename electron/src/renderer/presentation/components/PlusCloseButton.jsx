ae7e5d13f9b72265de8b8279558d30bc2eb04a0e
ikealmighty <isaacyates7@gmail.com>
2026-02-08 09:48:37 -0600

    added rotation animation

--- electron/src/renderer/presentation/components/PlusCloseButton.jsx
+++ electron/src/renderer/presentation/components/PlusCloseButton.jsx
@@ -1,7 +1,10 @@
 export default function PlusCloseButton({ value, onClick }) {
   return (
     <div className="text-4xl">
-      <button className="w-10 h-10 flex align-middle justify-center" onClick={onClick}>
+      <button
+        className={`align-middle justify-center ${value ? "animate-rotate-clockwise-45" : "animate-rotate-counterclockwise-45"}`}
+        onClick={onClick}
+      >
         &#10010;
       </button>
     </div>
